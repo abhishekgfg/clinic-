c647d1affcfa0bff15339a901544a36c20dbc01b
Abhishek Kumar <abhishek8579013@gmail.com>
2025-06-19 16:16:38 +0530

    s

--- Server/Routes/appointmentRoutes.js
+++ Server/Routes/appointmentRoutes.js
@@ -8,8 +8,8 @@ const {
   rescheduleAppointment,
 } = require("../Controllers/appointmentController");
 
-router.post("/add", addAppointment);
-router.get("/all", getAllAppointments);
+router.post("https://clinic-2uu2.onrender.com/api/appointments/add", addAppointment);
+router.get("https://clinic-2uu2.onrender.com/api/patients/all", getAllAppointments);
 router.patch("/:id/status", updateAppointmentStatus);
 router.delete("/:id", deleteAppointment);
 router.patch("/:id/reschedule", rescheduleAppointment);
